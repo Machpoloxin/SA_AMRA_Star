dac6dc0bdb10b313c3734bdc5f383d7a6af03968
Machpolo <machpolo.xin@gmail.com>
2024-05-05 20:16:35 +0200

    some_updates

--- AMRAstar.cpp
+++ AMRAstar.cpp
@@ -7,6 +7,7 @@
 #include "HeuristicManager.h"
 #include <array>
 #include <set>
+#include <chrono>
 
 
 /*
@@ -53,11 +54,17 @@ private:
     std::vector<std::set<Node>> openLists;  // for each heuristic, an openlist
     std::vector<std::vector<Node>> closeList;
     std::vector<Node> INCONS;
+    std::vector<Node> solutionPath;
     //std::vector<Node> Explored;
     HeuristicManager manager;
     int weight1;
     int weight2;
+    int weight1Step;
+    int weight2step;
     std::vector<std::pair<Resolution::Level, int>> heurs_map;
+    double startTime;
+    double searchTime;
+    double timeLimit; //ms
 
     int findNodePosition(const Node& node) {
         auto it = std::find(Explored.begin(), Explored.end(), node);
@@ -83,6 +90,7 @@ private:
         openLists[heuristicIndex].erase(node);
     }
 
+
     void updateOpenList(int heuristicIndex, const Node& node)
     {
         auto& openList = openLists[heuristicIndex];
@@ -97,6 +105,34 @@ private:
             addToOpenList(heuristicIndex, node);
         }
     }
+    
+    Node* findNodeByPosition(const std::vector<std::set<Node>>& openLists, int heuristicIndex, const std::array<int, 3>& position) 
+    {
+        auto& openList = openLists[heuristicIndex];
+        for (const auto& node : openList) {
+            if (node.position == position) {
+                return const_cast<Node*>(&node); // 小心使用 const_cast，仅当你确实需要修改元素时使用
+            }
+        }
+        return nullptr; // 如果未找到，则返回 nullptr
+    }
+
+    void removeFromOpenListByPosition(int heuristicIndex, const std::array<int, 3>& position) 
+    {
+        Node* node = findNodeByPosition(openLists, heuristicIndex, position);
+        if (node != nullptr) {
+            removeFromOpenList(heuristicIndex, *node);
+        }   
+    }
+
+    void printOPEN( int heuristicIndex)
+    {
+        for (auto const& opens: openLists[heuristicIndex])
+        {
+           std::cout << '(' << opens.position[0] << ',' << opens.position[1] << ',' <<opens.position[2]<< ',' << opens.h_cost << ')' <<std::endl;
+        }
+    }
+
 
     void clearAllOpen() 
     {
@@ -105,6 +141,17 @@ private:
             openList.clear();
         }
     }
+
+    std::set<Resolution::Level> getUniqueResolutionLevels(const std::vector<std::set<Node>>& openLists, size_t index) //also usable for closeLists 
+    {
+        std::set<Resolution::Level> uniqueLevels;
+        if (index < openLists.size()) {
+            for (const auto& node : openLists[index]) {
+                uniqueLevels.insert(node.res);
+            }
+    }
+    return uniqueLevels;
+    }   
 
     void addToCloseList(const Node& node, Resolution::Level res) 
     {
@@ -138,11 +185,21 @@ private:
         INCONS.clear();
     }
 
+    double getTime()
+    {
+        auto now = std::chrono::high_resolution_clock::now();
+        auto duration = now.time_since_epoch();
+        double milliseconds = std::chrono::duration<double, std::milli>(duration).count();
+        //std::cout << "Milliseconds since epoch: " << milliseconds << " ms" << std::endl;
+        return milliseconds;
+    }
+
 
 
 public:
     std::vector<Node> Explored;
-    AMRAstar(int initWeight1, int initWeight2, std::string path, std::array<int,3> taskStart, std::array<int,3> taskGoal, int resolutionScale): grid(path, resolutionScale),weight1(initWeight1),weight2(initWeight2)
+    AMRAstar(int initWeight1, int initWeight2, std::string path, std::array<int,3> taskStart, std::array<int,3> taskGoal, int resolutionScale, double timeLimit): 
+                                                                grid(path, resolutionScale),weight1(initWeight1),weight2(initWeight2),timeLimit(timeLimit)
     {
 
         //heurs_map.emplace_back(Resolution::ANCHOR, 0);//CAN CHANGE, 0: Heuristic Index!!!
@@ -161,6 +218,9 @@ public:
 
         start = {taskStart, Resolution::HIGH, 0, taskStartHvalue,0 + weight1 * taskStartHvalue, nullptr};
         goal = {taskGoal, Resolution::HIGH, taskGoalGvalue, 0, taskGoalGvalue + 0  , nullptr};
+        weight1Step = 0.1 * weight1;
+        weight2step = 0.1 * weight2;
+
     }
 
     double key(const Node& node, int i)
@@ -174,11 +234,20 @@ public:
     void expand(Node* node, int heuristicIndex) 
     {
         Resolution::Level res = heurs_map.at(heuristicIndex).first;
-        std::cout<< res<<std::endl;
+        std::vector<std::pair<std::array<int, 3>, std::string>> successors;
 
-        auto successors = grid.getSuccs(node->position, res);
-        grid.printNodes(successors);
-        grid.printBoundary();
+        //std::cout<< res<<std::endl;
+        if (node->position==goal.position)
+        {
+            successors.push_back({node->position, grid.getMapValue(node->position)});
+        }
+        else
+        {
+            successors = grid.getSuccs(node->position, res);
+        }
+        
+        //grid.printNodes(successors);
+        //grid.printBoundary();
 
         for (auto& successorPosition : successors) 
         {
@@ -243,7 +312,96 @@ public:
     }
 
 
+    bool improvePath(const double& startTime)
+    {
+        std::cout<<"count"<<std::endl;
+        bool checkImprove = false;
+        std::cout<<"size of Heuristics" << manager.countHeuristics() <<std::endl;
+        for (size_t i = 0; i < manager.countHeuristics(); ++i)
+        {
+            while (!openLists[i].empty()) 
+            { 
+                std::cout << i << std::endl;
+                double elapsedTime = getTime() - startTime; 
+                std::cout << "elapsedTime:" << elapsedTime << std::endl;
+                if (elapsedTime >= timeLimit)
+                {
+                    return false;
+                }
+                double fCheck = weight2 * openLists[0].begin()->f_cost;
+                if (openLists[i].begin()->f_cost > fCheck ) 
+                {
+                    i = 0;
+                }
+                Node x = *openLists[i].begin();
+                std::cout << "x_now: "<< '(' << x.position[0] << ',' << x.position[1] << ',' <<x.position[2]<< ',' << x.h_cost << ')' <<std::endl;
+                if (x.position == goal.position){
+                    std::cout << "beginToreconstruct"<<std::endl;
+                    std::cout << "x_parent: "<< '(' << x.parent->position[0] << ',' << x.parent->position[1] << ',' <<x.parent->position[2]<< ')' <<std::endl;
+                    //reconstructPath(x);
+                    checkImprove = true;
+                    return true;
+                }
+                removeFromOpenListByPosition(i, x.position);
+                std::cout << "Before_Opensize: " <<openLists[i].size() << std::endl;
+                expand(&x,i);
+                std::cout << "After_Opensize: " <<openLists[i].size() << std::endl;
+                std::cout << "open" << i << ':'<< std::endl;
+                printOPEN(i);
+                Resolution::Level res = heurs_map.at(i).first;
+                addToCloseList(x, res);
 
+            }
+        }
+        return checkImprove;
+    }
+
+    void reconstructPath(Node& node) {
+        solutionPath.clear();
+        for (Node* current = &node; current != nullptr; current = current->parent) {
+            solutionPath.push_back(*current);
+        }
+        std::reverse(solutionPath.begin(), solutionPath.end());
+        // Display or process the path
+    }
+
+    void search()
+    {
+        startTime = getTime();
+        clearAllOpen();
+        addToINCONS(start);
+        while((weight1 >= 1) && (weight2 >= 1))
+        {   
+            std:: cout << "weight1: "<< weight1<< std::endl;
+            std:: cout << "weight2: " <<weight2<< std::endl;
+            for(const auto& incon: INCONS){updateOpenList(0, incon);}
+            INCONS.clear();
+            std::set<Resolution::Level> ResolutionInOpenList = getUniqueResolutionLevels(openLists,0);
+            for (const auto& node_0: openLists[0])
+            {
+                for (size_t j = 0; j < manager.countHeuristics(); ++j)
+                {
+                    Resolution::Level res = heurs_map.at(j).first;
+                    if (ResolutionInOpenList.find(res)!= ResolutionInOpenList.end())
+                    {
+                        updateOpenList(j,node_0);
+                    }
+                }
+            }
+            clearAllClose();
+            if (improvePath(startTime))
+            {
+                for (const auto &solution: solutionPath)
+                {
+                    std::cout << '(' << solution.position[0] << ',' << solution.position[1] << ',' <<solution.position[2]<< ')' <<std::endl;
+                }
+                
+            }
+            weight1 = weight1 - weight1Step;
+            weight2 = weight2 - weight2step; 
+
+        }
+    }
 
 };
 
@@ -257,10 +415,13 @@ int main()
     // Start and goal positions
     std::array<int, 3> start = {1, 1, 0};
     std::array<int, 3> goal = {4, 1, 0};
+    //std::cout << start[0];
 
     // Initialize AMRAstar algorithm
-    AMRAstar amraStar(1, 1, "path_to_map_file", start, goal, 1);
-
+    AMRAstar amraStar(10, 10, "path_to_map_file", start, goal, 1, 1000);
+    /*
+    
+    
     // Node to expand
     Node testNode;
     testNode.position = start;
@@ -269,7 +430,7 @@ int main()
     testNode.h_cost = 0;
     testNode.f_cost = 0; // Assuming f_cost is g_cost + h_cost for simplicity
     testNode.parent = nullptr;
-    //std::cout<<"point1"<<std::endl;
+
     // Simulate expansion
     amraStar.expand(&testNode, 0);  // Use the first heuristic
 
@@ -278,7 +439,10 @@ int main()
     for (const auto& node : amraStar.Explored) {
         std::cout << "(" << node.position[0] << ", " << node.position[1] << ", " << node.position[2] << ") g_cost: " << node.g_cost << std::endl;
     }
+    */
 
+    amraStar.search();
+    
     return 0;
 }
 
